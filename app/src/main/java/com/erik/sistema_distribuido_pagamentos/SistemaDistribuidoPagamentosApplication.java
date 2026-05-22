f3a1737f25f607ae535960f9f30b33d6f76a60b0
Erik <erikdesouza.bue@gmail.com>
2026-05-22 15:34:11 -0300

    subindo springboot

---
+++ app/src/main/java/com/erik/sistema_distribuido_pagamentos/SistemaDistribuidoPagamentosApplication.java
@@ -0,0 +1,13 @@
+package com.erik.sistema_distribuido_pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDistribuidoPagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDistribuidoPagamentosApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/erik/sistema_distribuido_pagamentos/SistemaDistribuidoPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erik.sistema_distribuido_pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDistribuidoPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
